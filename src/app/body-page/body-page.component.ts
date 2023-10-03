6707a6f8be8875287dd17e20edf2619631f57d33
Giuseppe-Buscemi95 <giuseppe.b95@outlook.it>
2023-10-03 10:20:56 +0200

    ok

--- src/app/body-page/body-page.component.ts
+++ src/app/body-page/body-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit ,Input, OnChanges, SimpleChanges } from '@angular/core';
-import { of, map } from 'rxjs';
-import { interval } from 'rxjs';
+
 @Component({
   selector: 'app-body-page',
   templateUrl: './body-page.component.html',
