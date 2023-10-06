3c80f9449e8fadb4ea1bbe0514b01bf0ba94e32f
Giuseppe-Buscemi95 <giuseppe.b95@outlook.it>
2023-10-06 12:25:43 +0200

    probabilityMax e TagName

--- src/app/body-page/body-page.component.ts
+++ src/app/body-page/body-page.component.ts
@@ -5,30 +5,31 @@ import { HttpClient, HttpHeaders } from  '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { OnePieceAvatarService } from 'src/assets/services/one-piece-avatar.service';
 import * as base64js from 'base64-js';
-import { ArrayType } from '@angular/compiler';
-import { elementAt } from 'rxjs';
+
 @Component({
   selector: 'app-body-page',
   templateUrl: './body-page.component.html',
   styleUrls: ['./body-page.component.css'],
 })
-export class BodyPageComponent{
+export class BodyPageComponent {
   @ViewChild(ListenerDirective) listenerDirective:
     | ListenerDirective
     | undefined;
 
   uploadedImage: string | ArrayBuffer | null = null;
   fontIcon = faFileCirclePlus;
-  upl:Uint8Array[] = []
+  upl: Uint8Array[] = [];
   source: any[] = [];
- TagName: string[] = [];
- Probability: any[] = [];
+  TagName: string[] = []; //tutti i tag name 
+  Probability: any[] = []; // tutte le probabilità
+  ProbabilityMax: any[] = []; //il risultato con la probabilità più alta
+  numFixArr: any[] = [];
+  TagNameProbabilityMax: string = ''; //il nome con percentuale più alta
+  constructor(
+    private http: HttpClient,
+    public avatars: OnePieceAvatarService
+  ) {}
 
-
-  constructor(private http: HttpClient, public avatars: OnePieceAvatarService) {}
-
-
-  
   onDragDrop(event: FileList) {
     console.log(event[0].name);
     this.source.push(event[0].name); //pusho l'evento in un array any
@@ -36,13 +37,11 @@ export class BodyPageComponent{
     reader.readAsDataURL(event[0]); //permette di leggere i dati url
     reader.addEventListener('loadend', () => {
       this.uploadedImage = reader.result; //
-      this.CustomVisionAzure(this.uploadedImage) //POST CUSTOM VISION AZURE
+      this.CustomVisionAzure(this.uploadedImage); //POST CUSTOM VISION AZURE
     });
-   
   }
 
   onDragClick(event: Event) {
-
     let inputElement = event.target as HTMLInputElement; //casting
     const filePath = inputElement.files; // Ottieni il file selezionato
 
@@ -54,9 +53,8 @@ export class BodyPageComponent{
       reader.readAsDataURL(file); // Leggi il file come URL dati
       reader.addEventListener('loadend', () => {
         this.uploadedImage = reader.result; // Imposta l'immagine caricata
-        this.CustomVisionAzure(this.uploadedImage) //POST CUSTOM VISION AZURE
+        this.CustomVisionAzure(this.uploadedImage); //POST CUSTOM VISION AZURE
       });
-
     } else {
       // Nessun file selezionato, gestisci il caso appropriato qui
       console.log('Nessun file selezionato.');
@@ -64,50 +62,69 @@ export class BodyPageComponent{
   }
 
   //Passo immagine da analizzare tramite POST all'endpoint di Custom Vision di Azure
-  CustomVisionAzure(uploadimage: ArrayBuffer| string | null){
-    let byteArrayImage ;
+  CustomVisionAzure(uploadimage: ArrayBuffer | string | null) {
+    let byteArrayImage;
     let blobImage;
 
     console.log(uploadimage);
-   if (uploadimage instanceof ArrayBuffer) {
-     this.upl.push(new Uint8Array(uploadimage));
-   } else if(uploadimage != null) {
-    const base64WithoutPrefix = uploadimage.replace(/^data:image\/(jpeg|jpg);base64,/, ""); //rimozione del prefisso prima della conversione
-  
-     // Conversione da stringa64 ad array di byte, e successivo incapsulamento in blob, da passare a richiesta HTTP
-     byteArrayImage = base64js.toByteArray(base64WithoutPrefix);
-      blobImage = new Blob([byteArrayImage], { type: "base64" })
-     console.log(blobImage);
+    if (uploadimage instanceof ArrayBuffer) {
+      this.upl.push(new Uint8Array(uploadimage));
+    } else if (uploadimage != null) {
+      const base64WithoutPrefix = uploadimage.replace(
+        /^data:image\/(jpeg|jpg);base64,/,
+        ''
+      ); //rimozione del prefisso prima della conversione
 
-   }
+      // Conversione da stringa64 ad array di byte, e successivo incapsulamento in blob, da passare a richiesta HTTP
+      byteArrayImage = base64js.toByteArray(base64WithoutPrefix);
+      blobImage = new Blob([byteArrayImage], { type: 'base64' });
+      console.log(blobImage);
+    }
 
-   //variabili d'ambiente con valori API di Custom Vision
+    //variabili d'ambiente con valori API di Custom Vision
     const predictionKeyValue = environment.Value;
     const predictionKeyName = environment.Key;
-    
-  //httpHeader contiene metadati e valori predictionAPI
+
+    //httpHeader contiene metadati e valori predictionAPI
     const headers = new HttpHeaders()
-    .set('Content-Type', 'application/json; charset=utf-8')
-    .set(predictionKeyName, predictionKeyValue);
-  
-   
-    this.http.post<any>("https://imagesonepiececlassifications-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/c775cf70-e0d8-42d4-9d8f-a1c2b54ec55f/classify/iterations/Iteration2/image", blobImage , 
-    {headers}).subscribe((resp: any)=>{
-      let response = resp.predictions;
-      
-      for (let i = 0; i < response.length; i++) { this.TagName.push(response[i].tagName); }
-      for (let j = 0; j < response.length; j++){ this.Probability.push(response[j].probability);}
-    this.Probability.forEach(x => {
-      const ValoreABS = Number(x);
-      
-      console.log(ValoreABS.toFixed(2));
-    })
+      .set('Content-Type', 'application/json; charset=utf-8')
+      .set(predictionKeyName, predictionKeyValue);
 
-    }
-   
-    
+    this.http
+      .post<any>(
+        environment.url,blobImage,{ headers }
+      ).subscribe((resp: any) => {
+        let response = resp.predictions;
+        console.log(response);
+        
+        for (let j = 0; j < response.length; j++) {
+          this.Probability.push(response[j].probability);
+        }
+        this.Probability.forEach((x) => {
+          const ValoreABS = Number(x);
+          this.numFixArr.push(Number(ValoreABS.toFixed(2)));
+          const numMaz = this.numFixArr[0];
+          // console.log(this.numFixArr)
+          for (let a = 0; a < this.numFixArr.length; a++) {
+            const element = this.numFixArr[a];
+            //console.log(element);
+            if (numMaz > element) {
+              this.ProbabilityMax = numMaz.toFixed(2);
+              //console.log(numMaz);
+            } else if (numMaz < element) {
+              this.ProbabilityMax = element.toFixed(2);
+            }
+          }
+        });
+        for (let i = 0; i < response.length; i++) {
+          const probabilityString = this.ProbabilityMax.toString();
 
-    )
+          const ciao = response[i].probability.toFixed(2).toString();
+          if (ciao === probabilityString) {
+            this.TagNameProbabilityMax = response[i].tagName;
+          }
+          this.TagName.push(response[i].tagName);
+        }
+      });
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     Key: 'Prediction-Key',
-    Value: '9af8b026ac89410ab239ba2a38b9d6f9'
+    Value: '9af8b026ac89410ab239ba2a38b9d6f9',
+    url: 'https://imagesonepiececlassifications-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/c775cf70-e0d8-42d4-9d8f-a1c2b54ec55f/classify/iterations/Iteration2/image'
    
 };
