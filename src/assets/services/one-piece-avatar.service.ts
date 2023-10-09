1fd275b8e4e39d44c012ec25b23aae4c372c9863
AlexCoder99 <alexguci@outlook.it>
2023-10-09 16:55:53 +0200

    avatars

--- src/assets/services/one-piece-avatar.service.ts
+++ src/assets/services/one-piece-avatar.service.ts
@@ -9,16 +9,16 @@ export class OnePieceAvatarService {
 
   getAvatarImage(pgName: string):string {
     switch(pgName){
-      case 'Luffy':
+      case 'Monkey D. Luffy':
         return "../../assets/images/luffy_avatar.jpeg";
 
       case 'Roronoa Zoro':
         return "../../assets/images/zoro_avatar.jpg";
 
-      case 'Sanji':
+      case 'Vinsmoke Sanji':
         return "../../assets/images/sanji_avatar.jpeg";
 
-      case 'Tony Chopper':
+      case 'Tony Tony Chopper':
         return "../../assets/images/chopper_avatar.jpeg";
 
       case 'Nami':
@@ -30,11 +30,29 @@ export class OnePieceAvatarService {
       case 'Nico Robin':
         return "../../assets/images/nico_robin_avatar.jpeg";
 
-      case 'Aokijii':
+      case 'Franky':
+        return "../../assets/images/franky_avatar.jpeg";
+
+      case 'Brook':
+        return "../../assets/images/brook_avatar.jpeg";
+          
+      case 'Jinbe':
+        return "../../assets/images/jinbe_avatar.jpeg";
+
+      case 'Portgas D. Ace':
+        return "../../assets/images/ace_avatar.jpeg";
+
+      case 'Shanks':
+        return "../../assets/images/shanks-avatar.jpeg";
+
+      case 'Kuzan':
         return "../../assets/images/aokiji_avatar.jpeg";
 
       case 'Crocodile':
         return "../../assets/images/crocodile_avatar.jpeg";
+
+      case 'Dracule Mihawk':
+        return "../../assets/images/mihawk_avatar.jpeg";
 
       default: 
         return ""; 
